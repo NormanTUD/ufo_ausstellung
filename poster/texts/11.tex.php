2cef00b4cc6765da00942414c116e1c5f1c8c76d
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:28:13 +0200

    fix

--- poster/texts/11.tex.php
+++ poster/texts/11.tex.php
@@ -5,7 +5,7 @@
 bekannt ist, sich mit dem UFO-Phänomen, und darunter speziell
 Tierverstümmelungen, zu befassen. Sie ist überzeugt, die
 Tierverstümmelungen stammen sicher von Außerirdischen.</p>
-
+\qr[\wikipedia{} Linda Moulton Howe]{https://en.wikipedia.org/wiki/Linda_Moulton_Howe}
 </li>
 <li><p>Charles Hickson und Calvin Parker berichteten, dass sie am 11.
 Oktober 1973 beim Angeln an der Pascagoula River in Mississippi von
@@ -21,7 +21,8 @@ mögliche Alien-Kontakte. Skeptiker hinterfragten ihre Geschichten,
 wiesen auf Widersprüche hin und stellten die Authentizität ihrer
 Aussagen in Frage. Trotz der Kontroversen bleibt der Pascagoula-Fall ein
 bedeutendes Ereignis in der UFO-Geschichte.</p>
-
+		\qr[\wikipedia{} Pascagoula-Entführung]{https://en.wikipedia.org/wiki/Pascagoula_Abduction}
+		\qr[\youtube{} GEP-Video über den Pascagoula-Fall]{https://www.youtube.com/watch?v=gqJEE5u5QRM}
 </li>
 <li><p>Art Bell: Ein amerikanischer Radio-Host, vorallem bekannt für die
 Sendung <em>Coast-to-Coast-AM</em>, in der Anrufer mysteriöse
@@ -29,6 +30,7 @@ Geschichten erzählten. Darunter waren unter Anderem Mels Hole, aber auch
 viele UFO-relatierte Geschichten wie die Californian Drones oder ein
 angeblicher Anrufer, der in der Area 51 gearbeitet haben will.</p>
 
+		\qr[\wikipedia{} Art Bell]{https://en.wikipedia.org/wiki/Art_Bell}
 </li>
 <li><p>Vogel: Ein verschwommenes Bild eines Vogels, der als UFO
 fehlklassifiziert wurde.</p>
@@ -37,6 +39,7 @@ fehlklassifiziert wurde.</p>
 <li><p>London Alien: Angebliche Aliens, die in London aufgenommen worden
 sein sollen.</p>
 
+		\qr[UFO-Case-Book Eintrag]{https://www.ufocasebook.com/londonaliens.html}
 </li>
 </ul>
 <?php include('footer.php'); ?>
