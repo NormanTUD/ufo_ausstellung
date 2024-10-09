48a43d9e72588e1e256eac3d34d4075f18d0bbe2
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:33:02 +0200

    fix

--- poster/texts/15.tex.php
+++ poster/texts/15.tex.php
@@ -11,13 +11,17 @@ natürlich tat es das nicht und wurde dadurch noch mehr befeuert. Dann
 sind sie schnell zurückgerudert, und meinten, es sei ein Wetterballon
 gewesen. Dadurch wurde der Mythos geboren, die US-Regierung wisse mehr,
 als sie erzähle, und halte Erkenntnisse zum Thema UFO geheim.</p>
+		\qr[\wikipedia{} Thomas DuBose]{https://en.wikipedia.org/wiki/Thomas_DuBose}
 
+		\qr[\wikipedia{} Roswell-Zwischenfall]{https://en.wikipedia.org/wiki/Roswell_incident}
+		\qr[\youtube{} Internet Historian über Roswell]{https://www.youtube.com/watch?v=VT128ElBWkM}
 </li>
 <li><p>In den Jahren 1987 und 1988 soll Ed Waters in Gulf Breeze,
 Florida, dutzende Bilder von UFOs gemacht haben. Sie sahen sehr
 interessant aus, leider wurde jedoch bei ihm später ein Modell eines der
 UFOs auf dem Dachboden gefunden.</p>
 
+		\qr[\wikipedia{} Gulf-Breeze-Zwischenfall]{https://en.wikipedia.org/wiki/Gulf_Breeze_UFO_incident}
 </li>
 <li><p>Ein Bild veröffentlicht von der <em>Amalgamated Flying Saucer
 Clubs of America</em>, angeblich 1963 Von Paul Villa nahe Peralta,
@@ -25,12 +29,14 @@ Kalifornien (alternative Erzählungen sprechen von A. Villa Jr., der es
 in Albuquerque, New Mexico gesehen haben will) aufgenommen. Es wird
 allgemein als Fake gesehen.</p>
 
+		\qr[Zeitungsartikel über den Zwischenfall]{https://www.newspapers.com/article/albuquerque-journal-apolinar-a-villa-j/122308373/}
 </li>
 <li><p>Ein Bild aus der Reihe der sogenannten <em>McMinnville-UFOs</em>.
 die 1950 von Paul und Evelyn Trent in McMinnville, Oregon, USA
 aufgenommen worden sein sollen. Es stellte sich heraus, dass es ein
 hochgeworfener Rückspiegel war.</p>
 
+		\qr[\wikipedia{} McMinnville-UFO-Bilder]{https://en.wikipedia.org/wiki/McMinnville_UFO_photographs}
 </li>
 <li><p>Utsurobune: Die Utsurobune-Legende erzählt von einem
 ungewöhnlichen Vorfall im Jahr 1803 an der Küste der japanischen Provinz
@@ -42,7 +48,7 @@ Form einem modernen fliegenden Untertassen-Bericht ähnelt. Forscher wie
 Tanaka Kazuo haben diese Geschichte wegen ihrer Ähnlichkeiten zu
 späteren UFO-Sichtungen untersucht, obwohl die genauen Hintergründe
 ungeklärt bleiben.</p>
-
+\qr[Mehr Infos über Utsurobune]{https://www.nippon.com/en/japan-topics/g00879/}
 </li>
 <li><p>Das Nürnberger Himmelsspektakel von 1561 und das Basler
 Himmelsspektakel von 1566 sind bemerkenswerte historische Ereignisse,
@@ -58,7 +64,8 @@ kämpften, und wurde ebenfalls als apokalyptisches Omen gedeutet. Beide
 Ereignisse verdeutlichen die Vermischung von natürlichen Phänomenen,
 religiösen Überzeugungen und spekulativen Interpretationen der damaligen
 Zeit.</p>
-
+		\qr[\wikipedia{} Basler Flugblatt]{https://de.wikipedia.org/wiki/Basler_Flugblatt_von_1566}
+		\qr[\wikipedia{} Nürnberger Flugblatt]{https://de.wikipedia.org/wiki/N\%C3\%BCrnberger_Flugblatt_von_1561}
 </li>
 <li><p><em>Battle of Los Angeles</em>: Die sogenannte Schlacht um Los
 Angelesereignete sich in der Nacht vom 24. auf den 25. Februar 1942,
@@ -76,7 +83,7 @@ als Beispiel für Panik und Überreaktion während des Krieges zitiert.
 UFO-Verschwörungstheoretiker interpretieren ein Foto des Ereignisses als
 Beweis für eine außerirdische Präsenz, obwohl das Bild stark retuschiert
 war.</p>
-
+\qr[\wikipedia{} Battle of Los Angeles]{https://en.wikipedia.org/wiki/Battle_of_Los_Angeles}
 </li>
 <li><p>Bei der belgischen UFO-Welle, die 1989 begann, erlangte ein Foto
 eines dreieckigen UFOs mit Lichtern an den Ecken besondere
@@ -88,7 +95,7 @@ an den Ecken mit Glühbirnen versehen, bevor er es von einer Schnur
 hängend fotografierte. Trotz dieses Geständnisses und der Entlarvung des
 Bildes als Scherz hielt sich das Interesse an der UFO-Welle
 weiterhin.</p>
-
+\qr[\wikipedia{} Black Triangle UFOs]{https://en.wikipedia.org/wiki/Black_triangle_(UFO)}
 </li>
 <li><p>Das GIMBAL-Video, das erstmals 2015 von US-Kampfpiloten
 aufgenommen wurde, zeigt ein scheibenförmiges, schnell fliegendes Objekt
@@ -100,11 +107,13 @@ Pentagon die Echtheit der Aufnahmen bestätigte, erlangte das Video
 größere Aufmerksamkeit. Höchstwahrscheinlich waren es IR-Artefakte eines
 Flugzeug-Triebwerkes in der Entfernung.</p>
 
+		\qr[\youtube{} Lemmino über die Pentagon-Videos]{https://www.youtube.com/watch?v=SpeSpA3e56A}
 </li>
 <li><p>Ein angeblich 1968 in Russland abgestürztes UFO, aber eigentlich
 kommt das Bild aus der Doku <em>The Secret KGB UFO Files</em> und wurde
 nur falsch benannt unter der UFO-Szene immer weiter gegeben.</p>
 
+		\qr[IMDB-Eintrag zur Mockumentary The Secret KGB UFO Files]{https://www.imdb.com/title/tt0224072/}
 </li>
 </ul>
 <?php include('footer.php'); ?>
