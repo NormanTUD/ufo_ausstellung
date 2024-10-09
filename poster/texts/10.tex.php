9472b9f00afdfca5744896cb302e224e2eb0874e
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:27:14 +0200

    fix

--- poster/texts/10.tex.php
+++ poster/texts/10.tex.php
@@ -3,12 +3,12 @@
 <ul>
 <li><p>Das Avrocar, ein militärisches Testflugzeug in Form eines
 UFOs.</p>
-
+ \qr[\wikipedia{} Avrocar]{https://de.wikipedia.org/wiki/Avro_Canada_VZ-9AV}
 </li>
 <li><p>Das Triangle-UFO, das von einem militärischen Schiff geleakt
 wurde. Eigentlich waren es die Out-of-Focus seienden Warnlichter eine
 Flugzeuges oder eine Drohne.</p>
-
+\qr[Artikel über das Triangle-UFO]{https://www.tpr.org/science/2023-01-13/the-pentagon-got-hundreds-of-new-reports-of-ufos-in-2022-a-government-report-says}
 </li>
 <li><p>Rechts daneben Paul Bennewitz: ein amerikanischer Pilot, der in
 der Nähe eines Stützpunktes, auf dem geheime Tests durchgeführt worden
@@ -21,19 +21,21 @@ Außerirdischen in Dulce in einer Untergrundbasis gegen Menschen gekämpft
 und gewonnen haben. Bennewitz wurde in eine psychiatrische Klinik
 eingewiesen und starb dort vereinsamt und verarmt.</p>
 
+		\qr[\wikipedia{} Paul Bennewitz]{https://en.wikipedia.org/wiki/Paul_Bennewitz}
 </li>
 <li><p>Darunter das Titelblatt von <em>Project Grudge</em>, einem
 geheimen Forschungsprojekt der US-Armee, die versucht haben,
 UFO-Sichtungen zu erforschen.</p>
-
+ \qr[\wikipedia{} Project Grudge]{https://en.wikipedia.org/wiki/Project_Grudge}
 </li>
 <li><p>Rechts daneben General John A. Samford, der nach Roswell eine
 Pressekonferenz über den Absturz gegeben hat.</p>
 
+		\qr[\wikipedia{} General Samford]{https://en.wikipedia.org/wiki/John_A._Samford}
 </li>
 <li><p>Darunter ein Flare, eine militärische Leuchtrakete, die oft für
 UFO-Sichtungen sorgt.</p>
-
+\qr[\wikipedia{} Flares]{https://en.wikipedia.org/wiki/Flare}
 </li>
 </ul>
 <?php include('footer.php'); ?>
