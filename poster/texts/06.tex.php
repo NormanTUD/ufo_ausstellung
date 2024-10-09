ebb9d005784cf52c8c5c7c3a9e914b2614f13b7b
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:23:45 +0200

    fix

--- poster/texts/06.tex.php
+++ poster/texts/06.tex.php
@@ -7,23 +7,26 @@ Ozean aufgetaucht und weggeflogen sein. Die Bilder sendete er an eine
 schwedische UFO-Zeitschrift. In den hochauflösenden Bildern sieht man
 Angelschnur, die die Modelle hochhält.</p>
 
+		\qr[UFO-Case-Book zum Maslin-Beach-UFO]{https://www.ufocasebook.com/2012/mbphotos.html}
 </li>
 <li><p>Links ein Bild eines Video aus einer unbekannten Quelle. Es soll
 den Absturz eines UFOs in New Mexiko zeigen.</p>
-
+		\qr[UFO-Case-Book zum 1997-Crash]{https://www.ufocasebook.com/1997crashvideo.html}
+		\qr[\youtube{} Das Video selbst]{https://www.youtube.com/watch?v=cYsKUJtdtlg}
 </li>
 <li><p>Rechts daneben ein Bild, das Howard Menger, ein amerikanischer
 Kontaktler, angeblich von den Venusianern gemacht hat, die er getroffen
 haben will.</p>
-
+\qr[\wikipedia{} Howard Menger]{https://en.wikipedia.org/wiki/Howard_Menger}
 </li>
 <li><p>Darunter links: das Cecconi-UFO, das 1979 in Italien aufgenommen
 worden sein soll. Höchstwahrscheinlich war es ein Solarballon.</p>
-
+\qr[\wikipedia{} Solarballon]{https://de.wikipedia.org/wiki/Solarballon} \qr[Black-Vault-Artikel über den Cecconi-Fall]{https://www.theblackvault.com/casefiles/the-ufo-case-of-maresciallo-cecconi-june-18-1979/}
 </li>
 <li><p>Daneben zwei UFOs, die der Kontaktler Adamski angeblich gesehen
 haben will.</p>
 
+		\qr[\wikipedia{} George Adamski]{https://en.wikipedia.org/wiki/George_Adamski}
 </li>
 </ul>
 <?php include('footer.php'); ?>
