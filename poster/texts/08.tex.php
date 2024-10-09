8ddf97d0709a1cdcaf8798529cd29ef4420be78d
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:25:39 +0200

    fix

--- poster/texts/08.tex.php
+++ poster/texts/08.tex.php
@@ -6,6 +6,7 @@ Lentikularwolken als UFOs fehldeutet und dramatisch aufarbeitet.
 Eigentlich entstehen diese Wolkenformen durch Windströmungen, die
 feuchte Luft anheben.</p>
 
+		\qr[\wikipedia{} Lentikularwolken]{https://de.wikipedia.org/wiki/Lenticularis}
 </li>
 <li><p>Die beiden Bilder zeigen Sterne, die durch chromatische
 Aberration in ein buntes Spektakel verwandelt werden. Astronomen kennen
@@ -18,6 +19,7 @@ das, was einfach nur die Grenzen der Optik demonstriert, als
 Verschwörungstheorie verkauft wird. Man fragt sich, ob ein wirkliches
 UFO nicht vielleicht nur ein besonders verwirrter Stern ist!</p>
 
+		\qr[\youtube{} Heranzoomen an einen Stern]{https://www.youtube.com/watch?v=ZOwcvv034Ho} \qr[\wikipedia{} chromatische Aberration]{https://de.wikipedia.org/wiki/Chromatische_Aberration}
 </li>
 <li><p>Bild eines langen Wesens (<em>Flying Rod</em>): In einem
 Nachrichtenschnipsel aus Bagdad sehen wir ein langes Wesen, das als
@@ -30,6 +32,7 @@ vorstellen, wie sehr sich die Kritiker amüsieren, wenn sie die Debatte
 um die „überlegene Technologie“ der Flying Rods führen – eine
 Technologie, die offensichtlich nicht viel mehr ist als ein schnelles
 Insekt und ein unglückliches Kameraobjektiv.</p>
+		\qr[\wikipedia{} Flying Rods]{https://en.wikipedia.org/wiki/Rod_(optical_phenomenon)} \qr[\skeptoid{} Flying Rods]{https://skeptoid.com/episodes/4004}
 
 </li>
 <li><p>Lens Flares sind ein weiteres Phänomen, das oft für UFOs gehalten
@@ -42,7 +45,7 @@ ausreicht, um die Vorstellungskraft vieler zu beflügeln und sie zu einer
 intergalaktischen Debatte zu inspirieren. Vielleicht sollten wir den
 Lens Flare als neuen, beweisenden Beweis für UFOs einführen –
 schließlich war er schon in so vielen Filmen ein Star!</p>
-
+\qr[\wikipedia{} Lens-Flares]{https://en.wikipedia.org/wiki/Lens_flare}
 </li>
 <li><p>Darunter ein Bild einer Sternschnuppe, auch bekannt als Meteor,
 die schon seit Jahrhunderten die Menschen verzaubert haben und oft als
@@ -53,7 +56,7 @@ aus dem All, die in einem Hauch von Staub und Glanz verglühen, sind der
 perfekte Aufhänger für all jene, die das Außergewöhnliche suchen. Man
 fragt sich, wie oft eine gute Sternschnuppe für den nächsten großen
 UFO-Skandal verantwortlich ist.</p>
-
+\qr[\wikipedia{} Meteoren]{https://de.wikipedia.org/wiki/Meteor}
 </li>
 </ul>
 <?php include('footer.php'); ?>
