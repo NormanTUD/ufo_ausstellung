5649e3d69e1be0957aa3cbf1ad3fe1f91f76ed6c
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:26:12 +0200

    fix

--- poster/texts/09.tex.php
+++ poster/texts/09.tex.php
@@ -5,18 +5,21 @@
 Nashville gesichtet worden sein soll. In Wahrheit ist es ein Stück
 Bühnendekoration für ein <em>Electronic-Light-Orchestra</em>-Konzert
 (das zeigt das Bild darunter).</p>
-
+		\qr[UFO-Case-Book-Artikel über die 1989 Nashville UFO-Bilder]{https://www.ufocasebook.com/1989nashvillephotographs.html}
+		\qr[Artikel über die Konzerte von ELO und deren Props]{http://www.jefflynnesongs.com/ootbtour/}
 </li>
 <li><p>Rechts daneben ein Bild eines UFOs, von denen 1967 in England auf
 vielen Feldern welche aufgetaucht sind. Sie waren gefüllt mit schwarzer
 Flüssigkeit. Es stellte sich heraus: es war ein studentischer
 Scherz.</p>
 
+		\qr[\wikipedia{} 1967 British Flying Saucer Hoax]{https://en.wikipedia.org/wiki/1967_British_flying_saucer_hoax}
 </li>
 <li><p>Darunter ein Bild einer Voyager-Test-Sonde in der
 White-Sands-Missile-Range in New Mexiko. Es wird oft als Bild zum Thema
 UFO benutzt, ohne dass erklärt wird, was es wirklich war. Das Bild wurde
 offiziell von der NASA veröffentlicht.</p>
+		\qr[Artikel über Roswell, bei der das Bild erwähnt wird]{https://www.af.mil/The-Roswell-Report/}
 
 </li>
 <li><p>Darunter das sogenannte Calvine-UFO. Angeblich wurde es von einem
@@ -24,6 +27,7 @@ Jagdflugzeug verfolgt. Höchstwahrscheinlich ist es eine
 Fotomanipulation, und das UFO eigentlich die Spitze eines
 Berges.</p>
 
+		\qr[Artikel zum Calvine-UFO]{https://www.express.co.uk/news/weird/1947448/Calvine-ufo-breakthrough-solved}
 </li>
 </ul>
 <?php include('footer.php'); ?>
