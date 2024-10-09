7abf7542e4b2682833d695cd85f1f7b37a10c4f0
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:30:45 +0200

    fix

--- poster/texts/13.tex.php
+++ poster/texts/13.tex.php
@@ -8,13 +8,16 @@ ihm rauskamen. Die Schüler wurden von gläubigen UFO-Forschern gemeinsam
 interviewt und hatten so die unbewusste Gelegenheit, ihre Stories
 anzupassen. Es gibt viele, teilws widersrüchliche Zeichnungen von dem,
 was die Kinder gesehen haben wollen.</p>
-
+		\qr[\wikipedia{} Ariel-School-Zwischenfall]{https://en.wikipedia.org/wiki/Ariel_School_UFO_incident}
+		\qr[\hoaxilla{} Ariel-School-Zwischenfall]{https://hoaxilla.com/hoaxilla-321-aliens-in-zimbabwe/}
 </li>
 <li><p>Das Wesen, das angeblich bei der Kelly-Hopkinsville-Begegnung in
 Christian County, Kentucky (1955), eine Familie bedrängt haben soll,
 nachdem es mit seinem Raumschiff abgestürzt ist. Es stellte sich später
 heraus, dass es wahrscheinlich fauchende Eulen waren.</p>
-
+		\qr[\wikipedia{} Kelly-Hopkinsville-Encounter]{https://en.wikipedia.org/wiki/Kelly\%E2\%80\%93Hopkinsville_encounter}
+	\item Darunter das Bild eines Raumschiffes, das Lonnie Zamora gesehen haben 1964 will. Höchstwahrscheinlich ist er auf einen studentischen Scherz hereingefallen.
+		\qr[\skeptoid{} Lonnie Zamora]{https://skeptoid.com/episodes/4582}
 </li>
 <li><p>Darunter das Bild eines Raumschiffes, das Lonnie Zamora gesehen
 haben 1964 will. Höchstwahrscheinlich ist er auf einen studentischen
@@ -27,11 +30,13 @@ wie sie von Men in Black bedroht worden sind, nachdem sie gesehen haben,
 wie mehrere UFOs verschiedene Gegenstände haben fallen lassen sollen.
 Die Geschichte wird allgemein als Hoax verstanden.</p>
 
+		\qr[\wikipedia{} Maury-Island-Zwischenfall]{https://en.wikipedia.org/wiki/Maury_Island_incident}
 </li>
 <li><p>Das Flatwoods-Monster, das angeblich 1952 in Flatwoods, West
 Virginia, USA gesichtet worden sein soll. Der Aliensichtung ging eine
 UFO-Sichtung zuvor, die ein Flugzeugabsturz in der Nähe war.</p>
 
+		\qr[\wikipedia{} Flatwoods-Monster]{https://de.wikipedia.org/wiki/Flatwoods_Monster}
 </li>
 </ul>
 <?php include('footer.php'); ?>
