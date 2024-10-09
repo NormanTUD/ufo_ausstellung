9a2e1ec8677be99447762db0539bfa19786b4e76
Norman Koch <norman.koch@tu-dresden.de>
2024-10-09 10:18:56 +0200

    fix

--- poster/texts/04.tex.php
+++ poster/texts/04.tex.php
@@ -26,37 +26,45 @@ liefern keine Einsichten, sondern sind nur anhand der damals bekannten
 Naturkonstanten gefittete Formelwerte, die ohne mentales Modell
 auskommen. Seine Sprache ist extrem unverständlich, und seine Schriften,
 vorallem die esoterischen, kaum mehr lesbar.</p>
+		\qr[\wikipedia{} Burkhard Heim]{https://de.wikipedia.org/wiki/Burkhard_Heim}
 
+		\qr[\youtube{} Physiker Holm Hümmler analysiert die Behauptungen von Heim]{https://www.youtube.com/watch?v=lqR2S8yxfFM}
 </li>
 <li><p>George Adamski: einer der frühesten Kontaktler. Er behauptete,
 oft sogenannte Nordische Aliens getroffen zu haben und hat davon viele
 Fotos vorgelegt. Es stellte sich später heraus, dass er
 Alltagsgegenstände benutzte, um die UFOs zu inszenieren.</p>
 
+		\qr[\wikipedia{} George Adamski]{https://de.wikipedia.org/wiki/George_Adamski}
 </li>
 <li><p>Illobrand von Ludwiger: ein deutscher Physiker, der sich auf das
 UFO-Thema versteift hat und alles im Licht von Burkhard Heims betrachtet
 hat.</p>
 
+		\qr[\wikipedia{} Illobrand von Ludwiger]{https://de.wikipedia.org/wiki/Illobrand_von_Ludwiger}
 </li>
 <li><p>Billy Meier: ein schweizer Sektenführer, der behauptet, mit
 Plejadiern in Kontakt zu stehen.</p>
 
+		\qr[\wikipedia{} Billy Meier]{https://de.wikipedia.org/wiki/Eduard_Albert_Meier}
 </li>
 <li><p>Marshall Applewhite: Ein Sektenführer, der meinte, hinter dem
 Komet Hale Bopp sei ein UFO gewesen, das die Mitglieder der Sekte vor
 dem Weltuntergang retten sollte, indem sie ihren Körper verlassen. 39
 Mitglieder der Sekte haben sich daraufhin selbst getötet.</p>
 
+		\qr[\wikipedia{} Heavens Gate]{https://de.wikipedia.org/wiki/Heaven\%E2\%80\%99s_Gate_(Neue_Religi\%C3\%B6se_Bewegung)}
 </li>
 <li><p>Erich von Däniken: ein schweizer Autor, der alles Mögliche
 interpretiert als Zeichen für UFOs. Seine Thesen klingen auf den ersten
 Blick plausibel, halten aber Untersuchungen nicht stand.</p>
 
+		\qr[\wikipedia{} Erich von Däniken]{https://de.wikipedia.org/wiki/Erich_von_D\%C3\%A4niken}
 </li>
 <li><p>Kenneth Arnold: ein amerikanischer Pilot, der 1947 am Mount
 Rainier im Bundesstaat Washington neun fliegende, glänzende UFOs gesehen
 haben will.</p>
+		\qr[\wikipedia{} Kenneth Arnold]{https://de.wikipedia.org/wiki/Kenneth_Arnold}
 
 </li>
 <li><p>Claude Vorilhon: Gründer der UFO-Sekte Rael, die bekannt wurden
@@ -64,11 +72,13 @@ dadurch, dass sie Hitler klonen wollten. Angeblich forschen sie zum
 Thema Klonen und haben es angeblich bereits geschafft, Menschen zu
 klonen. Sie stehen angeblich im Kontakt mit Außerirdischen.</p>
 
+		\qr[\wikipedia{} Raelismus]{https://de.wikipedia.org/wiki/Raelismus}
 </li>
 <li><p>Lonnie Zamora: ein amerikanischer Polizist, der 1964 in der Wüste
 von New Mexiko ein Raumschiff gesehen haben. Wahrscheinlich war es ein
 Hoax einer Studentengruppe.</p>
-
+		\qr[\wikipedia{} Lonnie-Zamora-Zwischenfall]{https://en.wikipedia.org/wiki/Lonnie_Zamora_incident}
+		\qr[\skeptoid{} Lonnie-Zamora-Zwischenfall]{https://skeptoid.com/episodes/4582}
 </li>
 <li><p>Allen Hynek: ein amerikanischer Astronom, der bekannt wurde als
 einer der Vorreiter der UFO-Forschung. Von ihm kommt z. B. die
@@ -76,25 +86,29 @@ Klassifikation von UFO-Sichtungen, z. B. Nocturnal Light, Daylight Disc,
 Radar/Visual, Close Encounter of the first, second and third
 Kind.</p>
 
+		\qr[\wikipedia{} UFO-Forscher Hynek]{https://en.wikipedia.org/wiki/J._Allen_Hynek}
 </li>
 <li><p>Luis Elizondo: ein Whistleblower, der in einem Geheimprojekt des
 Pentagons für die Untersuchung von UFOs verantwortlich war. Er
 behauptete, die USA seien in Besitz von UFOs. Beweise konnte er keine
 vorlegen.</p>
 
+		\qr[\wikipedia{} Luis Elizondo]{https://en.wikipedia.org/wiki/Luis_Elizondo}
 </li>
 <li><p>Hans-Werner Peiniger: Gründer der GEP, der Gesellschaft für die
 Erforschung des UFO-Phänomens. Die GEP bearbeitet seit über 50 Jahren
 UFO-Fälle, die aus der gesamten Gesellschaft gemeldet werden und liefert
 seriöse Antworten auf die Frage, was Menschen möglicherweise gesehen
 haben könnten.</p>
-
+		\qr[\wikipedia{} Hans-Werner-Peiniger]{https://de.wikipedia.org/wiki/Hans-Werner_Peiniger}
+		\qr[Website der GEP]{https://www.ufo-forschung.de/}
 </li>
 <li><p>David Grusch: 2023 behauptete er, dass die US-Armee und auch
 Mussolini und einige weitere Regierungen UFOs geborgen hätten. Er sagte
 das unter Eid im US-Kongress aus, lieferte aber keine Beweise für seine
 Behauptungen, die insgesamt unglaubwürdig sind.</p>
 
+		\qr[\wikipedia{} David Grusch]{https://en.wikipedia.org/wiki/David_Grusch_UFO_whistleblower_claims}
 </li>
 </ul>
 <?php include('footer.php'); ?>
